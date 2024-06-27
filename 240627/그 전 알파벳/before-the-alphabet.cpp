a7e13f174d54519ee7e5e6b31345feda955f4287
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-06-27 14:25:09 +0900

    Title: 그 전 알파벳, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240627/그 전 알파벳/before-the-alphabet.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char x;
+    cin >> x;
+    
+
+    if(x == 'z'){
+        cout << 'a';
+    }
+    else{
+        cout << (char)(x - 1);
+    }
+    return 0;
+}
