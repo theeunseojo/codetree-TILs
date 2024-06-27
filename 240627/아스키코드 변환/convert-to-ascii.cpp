88942c129561d0857da7b529ac56970b3911d0ee
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-06-27 13:57:36 +0900

    Title: 아스키코드 변환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240627/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char a;
+    int b;
+
+    cin >> a  >> b;
+    int a_num = (int)a;
+    char b_char = (char)b;
+
+    cout << a_num << " ";
+    cout << b_char ;
+    return 0;
+}
