49ca847adb82eabf65d35f74b3a1e8a80d11ba79
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-06-27 13:52:10 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240627/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c,d,e;
+    cin >> a >> b >> c >> d >> e;
+    char a1 = (char) a;
+    char b1 = (char) b;
+    char c1 = (char) c;
+    char d1 = (char) d;
+    char e1 = (char) e;
+
+    cout << a1 << " ";
+    cout << b1 << " ";
+    cout << c1 << " ";
+    cout << d1 << " ";
+    cout << e1 << " ";
+    return 0;
+}
