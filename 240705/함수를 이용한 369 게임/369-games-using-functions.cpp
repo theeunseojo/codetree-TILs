c168ef3f3016d13e333be4c4ac34b72e81b1a56e
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-07-05 13:54:19 +0900

    Title: 함수를 이용한 369 게임, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240705/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 240705/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -5,17 +5,17 @@ bool Find(int i){
     int q  = 1;
     int r  = 0;
 
-    q = i / 10;
-    r = i % 10;
-
     while(q != 0){
+        q = i / 10;
+        r = i % 10;
         if(q == 3 || q == 6 || q == 9 || r == 3 || r == 6 || r == 9) return true;
         else{
-            q = q / 10;
+            if(q > 99) q = q / 10;
             r = q % 10;
+            if(q == 3 || q == 6 || q == 9 || r == 3 || r == 6 || r == 9) return true;
         }
-    }
     return false; 
+    }
 }
 
 bool IsMagicNum(int i){
