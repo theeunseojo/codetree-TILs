bfe61f793b1e8231302b5883789f3d6c9b3fa1c7
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-07-05 11:55:18 +0900

    Title: 함수를 이용한 369 게임, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240705/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+bool Find(int i){
+    int q  = 1;
+    int r  = 0;
+
+    q = i / 10;
+    r = i % 10;
+
+    while(q != 0){
+        if(q == 3 || q == 6 || q == 9 || r == 3 || r == 6 || r == 9) return true;
+        else{
+            q = q / 10;
+            r = q % 10;
+        }
+    }
+    return false; 
+}
+
+bool IsMagicNum(int i){
+    return i % 3 == 0 || Find(i);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a , b;
+    cin >> a >> b;
+
+    int cnt = 0;
+    for(int i = a; i <= b; i++){
+        if(IsMagicNum(i)) cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
