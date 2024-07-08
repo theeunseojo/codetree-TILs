c496300f6561f92baa720c7186ccf6fbf90b3184
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-07-08 13:34:21 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240708/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 240708/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -2,30 +2,25 @@
 using namespace std;
 
 // 수열 B가 수열 A의 연속 부분수열인지 판별하는 함수 
-bool IsSub(int a[] , int b[]){
-    int a_len = sizeof(a)  / sizeof(a[0]);
-    int b_len = sizeof(b) / sizeof(b[0]);
-    bool flag = true;
+bool IsSub(int a[] , int b[] , int a_len , int b_len){
+    bool flag = false;
 
     for(int i = 0; i < a_len; i++){
         for(int j = 0; j < b_len; j++){
             if(a[i + j] == b[j]){ // 두 배열이 같은 지 check
                 flag = true;
             }
-            else{
-                flag = false;
-            }
         }
     }
-    if(flag) return true;
-    else return false;
+    return flag;
 }
+
 int main() {
     // 여기에 코드를 작성해주세요.
     int n1, n2;
     cin >> n1 >> n2;
-    int a[101] = {0, };
-    int b[101] = {0, };
+    int a[n1];
+    int b[n2];
 
     for(int i = 0; i < n1; i++){
         cin >> a[i];
@@ -34,10 +29,10 @@ int main() {
     for(int i = 0; i < n2; i++){
         cin >> b[i];
     }
-
-    
     // 수열 B가 부분수열인지 판별
-    bool flag = IsSub(a, b);
+    int a_len = sizeof(a)  / sizeof(a[0]);
+    int b_len = sizeof(b) / sizeof(b[0]);
+    bool flag = IsSub(a, b , a_len, b_len);
     if(flag) cout << "Yes";
     else cout << "No";
     return 0;
