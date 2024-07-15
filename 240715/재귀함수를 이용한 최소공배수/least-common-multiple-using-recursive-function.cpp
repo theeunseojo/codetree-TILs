ef7c1b9e18d351b4dfe011e791fc8de35f64ed64
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-07-15 15:47:30 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240715/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
+++ 240715/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int gcd(int a, int b){
+    int num = 0;
+    for(int i = 1; i <= 10; i++){
+        if(a % i == 0 && b % i == 0){
+            num = i;
+        } 
+    }
+    return num;
+}
+
 int Lsm(int arr[], int n){
+
     if(n == 0) return 1;
     int lsm = Lsm(arr, n - 1); // 비교를 위해
-    if(lsm % arr[n] != 0) return lsm * arr[n];
-    return Lsm(arr, n -1);
+    if(lsm % arr[n] == 0) return Lsm(arr, n - 1);
+    return lsm * arr[n] / gcd(lsm, arr[n]); // 두수의 최소 공배수 구하기 
 }
 
 int main() {
