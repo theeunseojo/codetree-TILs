7e654cf39f111b7b748ddd8d0be5d41160de2cf6
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-07-15 16:46:45 +0900

    Title: 두 개의 동일한 수열, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240715/두 개의 동일한 수열/two-equal-series.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int a[100];
+    int b[100];
+
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        cin >> b[i];
+    }
+
+    sort(a, a + n);
+    sort(b, b + n);
+
+    bool flag  = false; // 원소를 check 
+
+    for(int i = 0; i < n; i++){
+        if(a[i] == b[i]) flag = true;
+    }
+    if(flag) cout << "Yes";
+    else cout << "No";
+    return 0;
+}
