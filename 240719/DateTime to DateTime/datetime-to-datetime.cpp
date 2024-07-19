304cac7a84095054530f38d2e1b4f37f2397c1b8
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-07-19 16:13:48 +0900

    Title: DateTime to DateTime, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240719/DateTime to DateTime/datetime-to-datetime.cpp
+++ 240719/DateTime to DateTime/datetime-to-datetime.cpp
@@ -8,6 +8,7 @@ int main() {
 
     //day -> min
     int month_to_min = (a - 11) * 24 * 60 ;
+
     if(month_to_min < 0) {
         cout << -1;
         return 0;
@@ -15,10 +16,8 @@ int main() {
 
     // hour calculate
     int elaspsed_time = (b * 60 + c) - (11 * 60 + 11);
-    if(elaspsed_time < 0) {
-        cout << -1;
-        return 0;
-    }
+    // if month is correct, we need to check the time
+    if(elaspsed_time < 0) (11 * 60 + 11) - (b * 60 + c);
 
     cout << month_to_min + elaspsed_time;
     return 0;
