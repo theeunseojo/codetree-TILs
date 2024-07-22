097f1b9a9de1cd8caf760b1383bb97f627a7da6c
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-07-22 15:43:53 +0900

    Title: 10진수로 변환하기, Time: 1082ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240722/10진수로 변환하기/convert-to-decimal.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int num = 0;
+    int i = 1;
+    while(true){
+        //마지막 n = 1 or 0일때 따로 처리해줘야함.
+        if(n == 1) {
+            num += i * n;
+            break;
+        }
+        num += i * (n % 10);
+        i *= 2;
+        n /= 10;
+    }
+
+    cout << num;
+    return 0;
+}
