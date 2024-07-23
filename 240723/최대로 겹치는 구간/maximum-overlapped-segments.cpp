18ddd3542978ae5b4c9ba2f1f86ce0bad2e07591
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-07-23 11:59:46 +0900

    Title: 최대로 겹치는 구간, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240723/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 240723/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -12,10 +12,8 @@ int main() {
     for(int i = 0; i < n; i++){
         int x1, x2;
         cin >> x1 >> x2;
-        if(x1 < 0 || x2 < 0){
-            x1 += 100;
-            x2 += 100;
-        }
+        x1 += 100;
+        x2 += 100;
         for(int j = x1; j < x2; j++){
             lines[j]++;
         }
