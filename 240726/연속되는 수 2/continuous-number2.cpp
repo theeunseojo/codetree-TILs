96bed147221b2d8c52d44009d067de4b7c5af3a2
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-07-26 11:58:17 +0900

    Title: 연속되는 수 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240726/연속되는 수 2/continuous-number2.cpp
+++ 240726/연속되는 수 2/continuous-number2.cpp
@@ -12,15 +12,16 @@ int main() {
         cin >> arr[i];
     }
 
-    // 횟수세기 
+    // 횟수세기
+    // case 1 : 같은 숫자 , case 2 : 다 다른 숫자 , case 3 : 섞임
     int cnt = 0;
     int max = 0;
     for(int i = 0; i < n; i++){
-        if(i == 0 || arr[i] != arr[i - 1]){
+        if(i == 0 || arr[i] != arr[i - 1]){ // 다를 경우 1로 초기화
             cnt = 1;
         }
         if(arr[i] == arr[i - 1]) cnt++; 
-        if(max < cnt) max = cnt;
+        if(max < cnt) max = cnt;            // 최대 횟수 구하는 max로 cnt 값 저장.
         
     }
     cout << max;
