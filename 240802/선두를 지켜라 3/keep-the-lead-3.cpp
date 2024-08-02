57202e052f3b527510df4423486c510e3def1bb3
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-08-02 19:38:19 +0900

    Title: 선두를 지켜라 3, Time: 12ms, Memory: 7MB, Status: Wrong Answer - CodeTree

--- 240802/선두를 지켜라 3/keep-the-lead-3.cpp
+++ 240802/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -17,7 +17,8 @@ int main() {
 
         // t번만큼 반복이므로 0부터 t -1까지 
         for(int j = 0; j < t; j++){
-            a[time++] = a[time - 1] + v;
+            a[time] = a[time - 1] + v;
+            time += 1;
         }
     }
 
@@ -31,34 +32,30 @@ int main() {
 
         // t번만큼 반복이므로 0부터 t -1까지 
         for(int j = 0; j < t; j++){
-            b[time++] = b[time - 1] + v;
+            b[time] = b[time - 1] + v;
+            time += 1;
         }
     }
+
     // A와 B 명예의 전당 기록 및 비교 
     // 선두가 바뀔 경우, 무조건 추가 이때 같은 경우도 고려 
     int cnt = 0;
     int leader = 3;
-    for(int i = 0; i <= time; i++){
+    for(int i = 1; i < time; i++){
         if(a[i] > b[i]){
-            // leader is b or leader is same 
-            if(leader == 2 || leader == 3) 
-                cnt++;
-                leader = 1;
-        
+            if(leader == 2 || leader == 3) cnt++;
+            leader = 1;
         }
         else if(a[i] == b[i]){
-            // leader is a or leader is b
-            if(leader == 1 || leader == 2) 
-                cnt++;
-                leader = 3;
+            if(leader = 1 || leader == 2) cnt++;
+            leader = 3;
         }
         else{
-            if(leader == 1 || leader == 3) 
-                cnt++;
-                leader = 2;
+            if(leader == 1 || leader == 3) cnt++;
+            leader = 2;
         }
     }
-    if(cnt == 0) cout << cnt + 1;
-    else cout << (cnt - 1); // 0초에서는 무조건 같으므로, 그 경우 제외 
+    if(cnt == 1) cout << cnt; //  선두가 바꾸지 않았을 경우 
+    else cout << cnt - 1;
     return 0;
 }
