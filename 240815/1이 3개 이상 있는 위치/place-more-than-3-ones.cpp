47132242c14a6dc891bdac605df7a3b8a247b8f8
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2024-08-15 16:20:56 +0900

    Title: 1이 3개 이상 있는 위치, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240815/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
+++ 240815/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
@@ -2,9 +2,14 @@
 #define MAX 101
 using namespace std;
 
+
+int n;
+bool InRange(int x, int y){
+    return (0 <= x && x < n && 0 <= y && y < n);
+}
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n;
+    
     cin >> n;
     
     int arr[MAX][MAX] = {};
@@ -14,15 +19,17 @@ int main() {
         }
     }
     // dx, dy technique
-    int dx[4] = {0, 1, 0, -1};
-    int dy[4] = {1, 0, -1, 0};
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, -1, 0, 1};
 
     int cnt = 0;
     int ans = 0;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             for(int dir_num = 0; dir_num < 4; dir_num++){
-                if(arr[i + dx[dir_num]][j + dy[dir_num]] == 1) cnt++;
+                int nx = i + dx[dir_num];
+                int ny = j + dy[dir_num];
+                if(InRange(nx, ny) && arr[nx][ny] == 1) cnt++;
                 if(cnt == 3) {
                     ans++;
                     break;
